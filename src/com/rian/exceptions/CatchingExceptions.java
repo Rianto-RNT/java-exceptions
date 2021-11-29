44fe9ec110d1ff898ccfc9d97e6ce832be5866e0
Rianto <rianto.rnt@gmail.com>
2021-11-29 09:27:06 +0800

    Catching Exceptions Java

--- src/com/rian/exceptions/CatchingExceptions.java
+++ src/com/rian/exceptions/CatchingExceptions.java
@@ -2,14 +2,9 @@ package com.rian.exceptions;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Locale;
 
-public class ExceptionDemo {
+public class CatchingExceptions {
     public static void show() {
-        sayHello(null);
-
-    }
-    public static void sayHello(String name) {
         try {
             var rader = new FileReader("file.txt");
         } catch (FileNotFoundException e) {
