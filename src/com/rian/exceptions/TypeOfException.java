0f6923ad43284a067b61a75fbc40015aac7117bd
Rianto <rianto.rnt@gmail.com>
2021-11-29 09:15:57 +0800

    Exceptions Hierarchy

---
+++ src/com/rian/exceptions/TypeOfException.java
@@ -0,0 +1,9 @@
+package com.rian.exceptions;
+
+import java.io.FileReader;
+
+public class TypeOfException {
+    public static void show() {
+        var reader = new FileReader("file.txt");
+    }
+}
