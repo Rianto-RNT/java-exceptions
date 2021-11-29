0976900dd1a90d742e7e4759ec46e85bb6833c42
Rianto <rianto.rnt@gmail.com>
2021-11-29 11:42:58 +0800

    The try with resources Statement

--- src/com/rian/exceptions/ExceptionDemo.java
+++ src/com/rian/exceptions/ExceptionDemo.java
@@ -1,27 +1,20 @@
 package com.rian.exceptions;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class ExceptionDemo {
     public static void show() {
-        FileReader reader = null;
 
-        try {
-            reader = new FileReader("file.txt");
+        try (
+                var reader = new FileReader("file.txt");
+                var writer = new FileWriter("...");
+
+        ) {
             var value = reader.read();
         } catch (IOException e) {
             System.out.println("Cloud not read data.");
-        }
-        finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
         }
     }
 }
