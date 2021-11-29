fe509f402404a45700b24e31d4ec0470088f7983
Rianto <rianto.rnt@gmail.com>
2021-11-29 09:05:13 +0800

    What are Exceptions

---
+++ src/com/rian/Main.java
@@ -0,0 +1,9 @@
+package com.rian;
+
+import com.rian.exceptions.ExceptionDemo;
+
+public class Main {
+    public static void main(String[] args) {
+        ExceptionDemo.show();
+    }
+}
---
+++ src/com/rian/exceptions/ExceptionDemo.java
@@ -0,0 +1,13 @@
+package com.rian.exceptions;
+
+import java.util.Locale;
+
+public class ExceptionDemo {
+    public static void show() {
+        sayHello(null);
+
+    }
+    public static void sayHello(String name) {
+        System.out.println(name.toUpperCase());
+    }
+}
