6d25def7bb5ea7866040574ba0aa4bb5a0d059dc
Rianto <rianto.rnt@gmail.com>
2021-11-29 11:33:05 +0800

    the finally Block

--- src/com/rian/exceptions/ExceptionDemo.java
+++ src/com/rian/exceptions/ExceptionDemo.java
@@ -2,18 +2,26 @@ package com.rian.exceptions;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 public class ExceptionDemo {
     public static void show() {
+        FileReader reader = null;
+
         try {
-            var reader = new FileReader("file.txt");
+            reader = new FileReader("file.txt");
             var value = reader.read();
-            new SimpleDateFormat().parse("");
-        } catch (IOException | ParseException e) {
+        } catch (IOException e) {
             System.out.println("Cloud not read data.");
         }
-    }
+        finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
 
+        }
+    }
 }
